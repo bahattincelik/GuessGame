f794a6f4d5e3a9788b6b5d2f69ee0f024e7dd623
Bahattin Celik <celik@wave-solutions.de>
2024-08-29 13:50:03 +0200

    first commit

---
+++ GuessingGame/Data/GameService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessingGame.Data
+{
+
+    public class GameService
+    {
+        private int _targetNumber;
+        private int _attempts;
+
+        public void StartNewGame()
+        {
+            _targetNumber = new Random().Next(1, 101);
+            _attempts = 0;
+
+        }
+
+        public string MakeGuess(int guess)
+        {
+            _attempts++;
+            if (guess < _targetNumber)
+            {
+                return "Too Low";
+            }
+            else if (guess > _targetNumber)
+            {
+                return "Too High";
+            }
+            else { return $"Correct!!! You guessed it in {_attempts} _attempts"; }
+        }
+
+        public int GetAttempts() => _attempts;
+    }
+}
---
+++ GuessingGame/Program.cs
@@ -0,0 +1,14 @@
+using GuessingGame.Data;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<GameService>();
+
+
+await builder.Build().RunAsync();
